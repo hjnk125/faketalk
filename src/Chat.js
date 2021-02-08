6b8ce9672ae3ea9bfc8ac16e637c67ad45e21216
김호정 <hjnk125@gmail.com>
2021-02-08 17:49:27 +0900

    Complete sender classification

--- src/Chat.js
+++ src/Chat.js
@@ -3,6 +3,7 @@ import './Chat.scss';
 
 function Chat(props) {
   const { name, pic } = props;
+  const [sender, setSender] = useState("user");
   const [newTalk, setNewTalk] = useState("");
   const [talks, setTalks] = useState([]);
 
@@ -11,16 +12,23 @@ function Chat(props) {
 
     let newTalks = talks.slice();
     // 새 톡 내용을 배열에 추가
-    newTalks.push(newTalk);
+    // [["user", "내가 말한 거(노랑 말풍선)"], ["friend", "친구가 말한거(흰 말풍선)"]]
+    let tuple = [sender, newTalk];
+    newTalks.push(tuple);
     setTalks(newTalks);
 
+    console.log(newTalks);
   }
+
+  function handleSender() {
+    (sender === "user") ? setSender("friend") : setSender("user");
+  };
 
   return (
     <div className="Chat">
 
       <div className="preview">
-        <div className="user">
+        <div className="user_info">
           <img src={pic} />
           <span>{name}</span>
         </div>
@@ -28,12 +36,13 @@ function Chat(props) {
         <ul className="talks">
           {talks.map((talk, i) => {
             return (
-              <li key={i}>{talk}</li>
+              <li key={i} className={talk[0]}>{talk[1]}</li>
             )
           })}
         </ul>
 
         <form className="send">
+          <input type="checkbox" onClick={handleSender} />
           <input type="text" name="talk" onChange={(e) => setNewTalk(e.target.value)} />
           <input type="submit" onClick={handleSubmit} />
         </form>
