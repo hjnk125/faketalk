42d8dca57756cf2fc3385e7fef47544c4c4fb449
김호정 <hjnk125@gmail.com>
2021-02-17 15:33:04 +0900

    Add "How to"

--- src/Setting.js
+++ src/Setting.js
@@ -15,11 +15,19 @@ function Setting(props) {
 
   return (
     <div className="Setting">
+      <div>
+        How to:<br />
+      1. 대화 전 톡을 나눌 친구의 이름과 프로필 사진을 설정해주세요. <br />
+      2. 전송버튼 위의 스위치를 이용해 톡을 보내는 사람을 설정할 수 있습니다. <br />
+      3. 사진을 전송할 때에는 사진을 업로드 후 전송버튼을 눌러주세요. <br />
+      </div>
+      <br />
       <div className="profile_name">
         <div>프로필 이름 변경:</div>
         <input type="text" onChange={(e) => setTempName(e.target.value)} />
         <input type="submit" onClick={(e) => { setName(tempName) }} />
       </div>
+      <br />
       <div className="profile_pic">
         <div>프로필 사진 변경:</div>
         <input type="file" accept="image/jpeg, image/jpg, image/png"
